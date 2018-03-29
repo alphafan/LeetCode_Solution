c6a28949f40bd786e70a26d2d1c079f72b8cf205
Yufan Zheng <yufan.zheng1@gmail.com>
2018-03-29 14:12:40 +0800

    056

---
+++ 056_Merge_Intervals.py
@@ -0,0 +1,32 @@
+"""
+Given a collection of intervals, merge all overlapping intervals.
+
+For example,
+Given [1,3],[2,6],[8,10],[15,18],
+return [1,6],[8,10],[15,18].
+"""
+
+
+# Definition for an interval.
+class Interval(object):
+
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+
+class Solution(object):
+
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+        intervals = sorted(intervals, key=lambda x: x.start)
+        merged = []
+        for interval in intervals:
+            if not merged or merged[-1].end < interval.start:
+                merged.append(interval)
+            else:
+                merged[-1].end = max(merged[-1].end, interval.end)
+        return merged
